1921b4d250315664f55798fa0e99fb3e54cd93e2
VLadzimir Kopanko <58780728+vovakopanko@users.noreply.github.com>
2023-03-17 17:54:31 +0100

    add schema for users and token

---
+++ models/user-model.js
@@ -0,0 +1,10 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema({
+  email: { type: String, require: true, unique: true },
+  password: { type: String, require: true },
+  isActivated: { type: Boolean, default: false },
+  activationLink: { type: String },
+});
+
+export default model("User", UserSchema);
